55c0f33f48165ee7a73a63db1bdfcbf20016402f
Aman Kumar <136639485+AmanLucifer@users.noreply.github.com>
2023-06-16 04:37:57 +0000

    var_con.js

--- demo.js
+++
@@ -1,7 +0,0 @@
-const userId = 12345
-let userName = "Aman"
-let userPassword = "xyz"
-let userNumber = "9905442618"
-let userState = "Bihar"
-
-console.table({userId, userName, userNumber,userPassword, userState})
