2883e3539a09efe585558cc2bcca55e66c1cf3ee
Aman Kumar <136639485+AmanLucifer@users.noreply.github.com>
2023-06-30 02:54:08 +0000

    Array

---
+++ 2_Basics/1_Array.js
@@ -0,0 +1,41 @@
+const MyArray = [0,1,2,3,4,5,] 
+
+// console.log(typeof MyArray);
+// console.log(MyArray);
+// console.log(MyArray[6]);
+
+let BookName = ["Maths", "Science", "English", "Computer"]
+// console.log(BookName)
+// console.log(BookName[1])
+
+
+
+//some Prototpe
+
+
+// MyArray.push("Summar")
+// MyArray.pop()
+// MyArray.unshift(55)
+// MyArray.shift()
+// MyArray.shift()
+
+// console.log(MyArray.includes(77)); //includes not includes it means data ha ya na.
+// console.log(MyArray.indexOf("Aman"));
+// console.log(MyArray.indexOf("aman")); // when -1 one is sowing you understand the data is not included.
+
+// const ArrayName = MyArray.join()
+// console.log( typeof ArrayName);
+// console.log( typeof MyArray);
+
+
+console.log("A", MyArray);
+
+let =  FirstArray = MyArray.slice(1,4);
+console.log(FirstArray);
+
+
+console.log("B", MyArray);
+let =  SecondArray = MyArray.splice(1,4);
+console.log("Left", MyArray);
+console.log(SecondArray);
+
