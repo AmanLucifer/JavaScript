cfc5346a6e7b30f7d60cd4a5843e8110384669f0
Aman Kumar <136639485+AmanLucifer@users.noreply.github.com>
2023-07-01 14:50:17 +0000

    Objects

---
+++ 2_Basics/3_object.js
@@ -0,0 +1,33 @@
+//objects
+const firstsymbol = Symbol("Good")
+
+const firstobject = {
+    name: "Aman", 
+    class: 8,
+    age: 14,
+    email: "aman@gmail.com",
+    Islogin: true,
+    "UserLocation": "India",
+    [firstsymbol]: "sym"  //this is symbol it is use in object. 
+}
+
+//value ==> name, class, age, email, Islogin 
+//value is store as string 
+//key ==> aman, 8, 14, aman@gmail.com, true
+//kay is store as string, number, boolen as your wish.
+
+
+console.log(firstobject);
+console.log(firstobject.email);
+console.log(firstobject["email"]);
+console.log(firstobject.UserLocation);
+console.log(firstobject[firstsymbol]);
+
+//how to changr key in object.
+
+firstobject.email = "hero@gmail.com" // chande email, name and more ya override
+// if you want to not change email, name and many more use this.
+Object.freeze(firstobject)
+firstobject.email = "ram@gmail.com"
+console.log(firstobject);
+
