e83c7b31e3929a193c0a0eeeb3e4f15f451a545a
Aman Kumar <136639485+AmanLucifer@users.noreply.github.com>
2023-07-09 12:22:44 +0000

    Functions-Projects

---
+++ 3_Basics/2_functions-project.js
@@ -0,0 +1,31 @@
+
+
+//Additions
+function add(num1 , num2){
+    return num1 + num2
+
+}
+
+//Subratiton
+function sub(num1 , num2){
+    return num1 - num2
+
+}
+
+
+//Multiplication 
+function multilply(num1 , num2){
+    return num1 * num2
+
+}
+//Division 
+function divide(num1 , num2){
+    return num1 / num2
+
+}
+
+
+console.log(add(5,7))  //output ==> 12
+console.log(sub(5 ,7))  //output ==> -2
+console.log(multilply (5 ,7))  //output ==> 35
+console.log(divide(10,5))  //output ==> 2
