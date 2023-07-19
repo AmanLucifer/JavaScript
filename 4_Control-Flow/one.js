8882fd04bad1c76de337a6e3a790d98620e0b986
Aman Kumar <136639485+AmanLucifer@users.noreply.github.com>
2023-07-19 10:46:26 +0000

    if-else-and-or

---
+++ 4_Control-Flow/one.js
@@ -0,0 +1,68 @@
+//if
+// const userLogin = true
+// if (userLogin) {
+//     console.log("Hii Your are Login");
+// } else{
+//     console.log("Please Login");
+// }
+
+
+// const Temprature = 40
+
+// if (Temprature < 40) {
+//     console.log("Temprature High");
+// }else{
+//     console.log("Temprature low");
+// }
+
+
+// <, >, ==,<=, =>, ===, !=, !==   (oprater)
+
+
+// const score = 200
+
+// if (score > 100){                           
+//     let  power = "fly"                           
+//     console.log(`you can: ${power}`);   //var is also work out of scope is not good is liya use let.
+
+// }
+//  console.log(`you can: ${power}`);
+
+const amount = 2000
+
+// if (amount > 1000 ) console.log("good"),console.log("Secondgood");  //i am not prefere to use this code 
+//how to use to or more condition.
+
+
+// if (amount < 500) {
+//     console.log("Amount is 500");
+// }else if (amount < 1000){
+//     console.log("Amount is 1000");
+// }else if (amount < 1500){
+//     console.log("Amount is 1500");
+// }else if (amount < 2100){
+//     console.log("Amount is 2100");
+// }
+
+
+//Real-stice Example of use multypal condition.
+//this is and condition.
+
+
+// const userName = true
+// const userEmail = true
+
+// if (userName && userEmail && 2==2) {
+//     console.log("You Have To Buy This Course");
+    
+// }
+
+
+//this is or condation.
+const mobileNumber = true
+const EmailId = true
+
+if (mobileNumber || EmailId) {
+    console.log("Allow to Access");
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# JavaScript
-Learning of JavaScript 
+
