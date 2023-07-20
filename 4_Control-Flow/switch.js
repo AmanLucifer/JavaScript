972d8e4324124423869965ea0d126f83f93d62d1
Aman Kumar <136639485+AmanLucifer@users.noreply.github.com>
2023-07-20 10:58:07 +0000

    switch

---
+++ 4_Control-Flow/switch.js
@@ -0,0 +1,52 @@
+//check multipal condition.
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+//****************this is for number***************//
+// const week = 5
+// switch (week) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+
+//     default:
+//         console.log(" Sorry (: Not match");
+//         break;
+// }
+
+//****************this is for string***************//
+// const name = "Aman"
+// switch (name) {
+//     case "rahul":
+//         console.log("No match");
+//         break;
+//     case "rahul":
+//         console.log("No match");
+//         break;
+//     case "aman":
+//         console.log("WOW!!! yes Aman is present");
+//         break;
+
+//     default:
+//         break;
+// }
