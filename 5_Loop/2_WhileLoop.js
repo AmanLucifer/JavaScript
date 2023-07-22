fbf1b4e8378f7734a92896a0612803196aef4120
Aman Kumar <136639485+AmanLucifer@users.noreply.github.com>
2023-07-22 16:23:38 +0000

    while loop

---
+++ 5_Loop/2_WhileLoop.js
@@ -0,0 +1,41 @@
+//while loop
+let  Number = 0
+while (Number <=10) {
+    console.log(`This is value ${Number}`);
+    
+    Number = Number + 2
+    
+}
+
+
+// while loop with array
+const ThisArray = ["keyboard", "mouse", "cpu", "Memory", "RAM"] 
+
+let arr = 0
+while (arr < ThisArray.length) {
+    console.log(ThisArray[arr]);
+    arr = arr +1
+    
+}
+
+
+// do while loop
+
+let num = 0
+do {
+    console.log(`value ${num}`);
+    num = num +1
+    
+} while (num <=5);
+
+
+
+// let num = 6
+// do {
+//     console.log(`value ${num}`);
+//     num = num +1
+    
+// } while (num <=5);
+
+
+//output is 6 because first console after checking condition okkkk.
