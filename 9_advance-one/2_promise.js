06336dfc12f4cb3065388b40b9ad62dbe35a334a
Aman Kumar <136639485+AmanLucifer@users.noreply.github.com>
2023-10-22 16:30:32 +0000

    Promise in depth

--- 9_advance-one/2_promise.js
+++ 9_advance-one/2_promise.js
@@ -11,5 +11,65 @@ Onepromise.then(() => {
 })
 
 
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Task 2 complete");
+        resolve()
+    }, 1000)
+}).then(() => {
+    console.log("Task 2 resolve");
+})
 
-console.log("amam");
+
+
+const PromiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ username: "Aman", email: "mr.amanport@gmail.com" })
+    }, 1000)
+})
+
+PromiseThree.then(function (UserData) {
+    console.log(UserData);
+})
+
+
+const PromiseFour = new Promise(function (resolve, reject) {
+    let error = true
+    if (!error) {
+        resolve({
+            username: "Aman Kumar",
+            email: "example@gmail.com"
+        })
+    } else {
+        reject("Error: something went wrong")
+    }
+})
+
+PromiseFour.then(function (user) {
+    console.log(user);
+    return user.email
+})
+    .then(function (useremail) {
+        console.log(useremail);
+    })
+    .catch(function (error) {
+        console.log(error);
+
+    })
+    .finally(function(){
+        console.log("the promise is resolve or reject");
+    })
+
+
+fetch('https://api.github.com/users/AmanLucifer')
+    .then((responce) => {
+        const data = responce.json()
+        return data
+    })
+    .then((data) => {
+        console.log(data.id);
+    })
+    .catch(() => {
+        console.log("Err");
+    })
+   
